71383e9586aa74a8de23c2ad31388d8db78afe40
Lapo Marchionni <marchionni.lapo@gmail.com>
2016-11-03 16:11:49 +0100

    Update sse_bitcount.cpp

--- sse_bitcount.cpp
+++ sse_bitcount.cpp
@@ -1,29 +1,24 @@
-#include <iostream> 	// input output
-#include <chrono>		// timer
-#include <cstdlib>		// random
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
 
-#include "xmmintrin.h"	// SSE 
-#include "smmintrin.h"	// SSE
-#include "emmintrin.h"	// SSE
+#include "xmmintrin.h"
+#include "smmintrin.h"
+#include "emmintrin.h"
 
-// Prender in input una dimensione
-// creare un vettore di questa dimensione
-// inizializzare il vettore
-// contare i bit 
 int main (int argc, char* argv[]) {
-	// Legge il numero di elementi da linea di comando
-	// Check if numero di argomenti pari a 2
+
 	if (argc != 2) {
 		std::cout << " !! Error !! " << std::endl;
 		std::cout << " !! Usage: !! " << std::endl;
-		std::cout << "\t ./exe1 <num_elements> " << std::endl;
+		std::cout << "\t ./sse_bitcount <vector_size> " << std::endl;
 		return 1;
 	}
 
-	long size = atol(argv[1]); // ASCII to long
-	std::cout << "Num elements: " << size << std::endl;
+	long size = atol(argv[1]);
+	std::cout << "Number of elements: " << size << std::endl;
 
-	// Creare un vettore di dimensione size
+	/* Create vector long <vector_size> */
 	int* buf = (int*) _mm_malloc(size*sizeof(int), 16);
 	if (!buf) {
 		std::cout << " !! Error while allocating memeory" << std::endl;
@@ -32,15 +27,14 @@ int main (int argc, char* argv[]) {
 
 	std::cout << "Size in MB " << size*sizeof(int)/1000/1000 << std::endl;
 
-	// Inizializare il vettore
+	/* Fill the vector with random integers */
 	for (long i=0; i<size; i++) {
 		buf[i] = rand();
 	}
-
-	// Initialize count
+	
 	auto begin = std::chrono::high_resolution_clock::now();
 
-	// Conto i bit settati ad 1
+	// Count set bits with sideways addition
 	__m128i sse_bitcount = _mm_set1_epi32(0);
 	
 	float* p_sse = (float*) buf;
@@ -137,16 +131,13 @@ int main (int argc, char* argv[]) {
 	_mm_storeu_ps((float*) tmp, _mm_castsi128_ps(sse_bitcount));
 	long count = tmp[0] + tmp[1] + tmp[2] + tmp[3];
 
-	// End timer
 	auto end = std::chrono::high_resolution_clock::now();
 	auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin);
 
-	// Stampo i risultati
 	std::cout << "Bit count: " << count << std::endl;
 	std::cout << "Time     : " << elapsed.count() << "ms." << std::endl;
 	std::cout << std::endl;
 
-	// De alloco memeoria
 	_mm_free(buf);
 	return 0;
 } 
